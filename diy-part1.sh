73cad30e589fd3c4c9a8c612b7f61230e9fade6d
olsl <olsl@qq.com>
2026-09-03 06:45:16 +0800

    fix: copy kst_wf3000a DTS to expected path for 6.6 build

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,10 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+
+# Fix: copy kst_wf3000a DTS to expected path for 6.6 kernel build
+# filogic.mk uses DEVICE_DTS_DIR := $(DTS_DIR)/mediatek, expecting DTS under
+# target/linux/mediatek/dts/mediatek/, but it only ships in files-6.6/
+mkdir -p target/linux/mediatek/dts/mediatek
+cp -f target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7981-kst-wf3000a.dts target/linux/mediatek/dts/mediatek/
+cp -f target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7981.dtsi target/linux/mediatek/dts/mediatek/
