d7c3087cd41684ac945856a3356070912c148f3a
olsl <olsl@users.noreply.github.com>
2026-09-06 07:12:23 +0800

    Add KJ30-N build workflow (dailook 24.10-6.6)

---
+++ diy-part1-kj30n.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1-kj30n.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# Note: dailook/immortalwrt-mt798x-6.6 already includes built-in support
+# for KJD KJ30-N (dts + filogic.mk device definition), so no device
+# injection is needed here.
+#
