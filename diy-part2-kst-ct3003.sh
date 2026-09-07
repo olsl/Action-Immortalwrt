9ed1881cc28e2d54cb3c9a847185b4ff05b42292
olsl <olsl@users.noreply.github.com>
2026-09-07 17:27:37 +0800

    fix(ct3003-kst): patch mt_wifi ap_cfg.c/cmm_cfg.c missing wtbl_update_pwr_offset declaration
    启用 MGMT_TXPWR_CTRL 后，ap_cfg.c:3832 / cmm_cfg.c:12064,12112 调用
    wtbl_update_pwr_offset，其声明仅在 include/mac/mac_mt/fmac/mt_fmac.h
    （受 MGMT_TXPWR_CTRL 保护），两文件 include 链不含该头文件，
    触发 -Werror=implicit-function-declaration。
    diy-part2 中补与 mt_fmac.h 一致的前向声明（C 允许重复声明）。

--- diy-part2-kst-ct3003.sh
+++ diy-part2-kst-ct3003.sh
@@ -7,6 +7,21 @@ rm -rf feeds/packages/lang/rust
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
+# Fix mt_wifi ap_cfg.c/cmm_cfg.c implicit declaration of wtbl_update_pwr_offset
+# (2410 分支源码缺陷：声明位于 include/mac/mac_mt/fmac/mt_fmac.h（受 MGMT_TXPWR_CTRL
+# 保护），但 ap_cfg.c/cmm_cfg.c 的 include 链（rt_config.h）不包含该头文件，
+# 启用 CONFIG_MTK_MGMT_TXPWR_CTRL 后即触发 -Werror=implicit-function-declaration。
+# 补与 mt_fmac.h 一致的前向声明，C 允许重复声明，不影响后续 include)
+MT_WIFI_DIR=package/mtk/drivers/mt_wifi/src/mt_wifi
+for f in embedded/ap/ap_cfg.c embedded/common/cmm_cfg.c; do
+  grep -q "wtbl_update_pwr_offset" $MT_WIFI_DIR/$f || continue
+  sed -i '/^#include "rt_config.h"/a\
+#ifdef MGMT_TXPWR_CTRL\
+INT wtbl_update_pwr_offset(struct _RTMP_ADAPTER *pAd, struct wifi_dev *wdev);\
+#endif' $MT_WIFI_DIR/$f
+done
+
+
 # Modify filename, add date prefix
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
 
